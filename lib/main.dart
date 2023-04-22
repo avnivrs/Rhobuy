907465072aa901caf7520f583a72b78d123a85f5
christian-iuil <okoriec01@gmail.com>
2023-04-22 21:07:36 +0100

    animated text

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 987),
+      designSize: const Size(375, 812),
       builder: (BuildContext context, Widget? child) {
         return GetMaterialApp(
           title: 'Flutter Demo',
